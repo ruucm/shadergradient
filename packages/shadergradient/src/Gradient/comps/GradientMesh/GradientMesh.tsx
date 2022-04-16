daf27715790ac7419945f80c93b1c880fc8c8a42
ruucm <ruucm.a@gmail.com>
2022-04-17 04:11:49 +0900

    add waving shader!

--- packages/shadergradient/src/Gradient/comps/GradientMesh/GradientMesh.tsx
+++ packages/shadergradient/src/Gradient/comps/GradientMesh/GradientMesh.tsx
@@ -5,29 +5,44 @@ import * as THREE from 'three'
 import { useFiber } from '../../../useFiber'
 import fragment from './glsl/shader.frag'
 import vertex from './glsl/shader.vert'
+import { shaderMaterial } from './shaderMaterial'
+
+const meshCount = 192
+const clock = new THREE.Clock()
 
 export const GradientMesh: React.FC<any> = () => {
-  const { useFrame, extend, shaderMaterial } = useFiber()
+  const { useFrame, extend } = useFiber()
 
   const ColorShiftMaterial = shaderMaterial(
     {
       time: 0,
       color: new THREE.Color(0.05, 0.0, 0.025),
+      uTime: { value: 0 }, // should be a object that has value to use in the shader
+      uNoiseDensity: 2,
+      uNoiseStrength: 3,
     },
     vertex,
     fragment
   )
   extend({ ColorShiftMaterial })
 
-  const mesh = useRef()
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
+  const material = useRef()
+  useFrame((state, delta) => {
+    // mesh.current.rotation.x += 0.01
+    material.current.uniforms.uTime.value = clock.getElapsedTime()
+  })
 
   return (
-    <mesh ref={mesh} rotation={[0.3, 0.3, 0]}>
-      <boxGeometry args={[1, 1, 1]} />
+    <mesh rotation={[0.3, 0.3, 0]}>
+      <planeGeometry args={[10, 10, meshCount, meshCount]} />
+      {/* <boxGeometry args={[1, 1, 1]} /> */}
       {/* <meshStandardMaterial color={'gold'} /> */}
       {/* @ts-ignore */}
-      <colorShiftMaterial key={ColorShiftMaterial.key} time={3} />
+      <colorShiftMaterial
+        key={ColorShiftMaterial.key}
+        time={3}
+        ref={material}
+      />
     </mesh>
   )
 }

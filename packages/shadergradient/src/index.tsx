65320c2490f29bd16573af5c64f565a07dc1442d
ruucm <ruucm.a@gmail.com>
2022-04-17 02:57:32 +0900

    clean up!

--- packages/shadergradient/src/index.tsx
+++
@@ -1,4 +0,0 @@
-export { Button } from './Button/index' // example component
-export { FiberCanvas, FiberContextProvider } from './useFiber'
-export { Gradient } from './Gradient/index'
-export { GradientMesh } from './Gradient/comps/GradientMesh/index'
--- packages/website/README.md
+++
@@ -1,3 +0,0 @@
-# website
-
-This project was bootstrapped with [`create-r3f-app`](https://github.com/utsuboco/create-r3f-app)

98d266aeef23379f203345eef780a9d2a8f7a254
ruucm <ruucm.a@gmail.com>
2022-04-17 04:08:19 +0900

    fmt

--- packages/shadergradient/src/index.tsx
+++ packages/shadergradient/src/index.tsx
@@ -1,4 +1,4 @@
 export { Button } from './Button/index' // example component
-export { FiberCanvas, FiberContextProvider } from './useFiber'
+export * from './useFiber'
 export { Gradient } from './Gradient/index'
 export { GradientMesh } from './Gradient/comps/GradientMesh/index'

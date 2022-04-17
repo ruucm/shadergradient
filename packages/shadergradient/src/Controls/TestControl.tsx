4fea439c491c61defa528d1c4672f1a3fd79b86f
ruucm <ruucm.a@gmail.com>
2022-04-17 14:41:24 +0900

    fix fontSize of the input

--- packages/shadergradient/src/Controls/TestControl.tsx
+++ packages/shadergradient/src/Controls/TestControl.tsx
@@ -10,7 +10,7 @@ export function TestControl() {
       onChange={(e) => {
         setUStrength(e.target.value)
       }}
-      style={{ border: '1px solid black' }}
+      style={{ border: '1px solid black', fontSize: 24 }}
     />
   )
 }

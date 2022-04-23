26f5edada8df9cab08dce259f886ce571a276216
deploy <deploy@users.noreply.github.com>
2022-04-23 15:54:24 +0000

    Updated

---
+++ shadergradient@0.1.12/Controls/index.js
@@ -0,0 +1,5 @@
+// src/Controls/index.ts
+export * from "./TestControl.js";
+export * from "./NumberInput/index.js";
+export * from "./Radio.js";
+export * from "./ShapeControls.js";
--- shadergradient@0.1.12/index.js
+++ shadergradient@0.1.12/index.js
@@ -2,10 +2,8 @@
 import { Button } from "./Button/index.js";
 export * from "./useFiber.js";
 export * from "./Gradient/index.js";
-export * from "./Controls/TestControl.js";
+export * from "./Controls/index.js";
 export * from "./hooks/index.js";
-import { NumberInput } from "./NumberInput/index.js";
 export {
-  Button,
-  NumberInput
+  Button
 };

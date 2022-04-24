b7b6c918283eb92665b9c0f71ebda27322124a9a
ruucm <ruucm.a@gmail.com>
2022-04-24 14:13:14 +0900

    Customizing server behavior

--- packages/shadergradient/esmbuild/esmbuild.js
+++ packages/shadergradient/esmbuild/esmbuild.js
@@ -1,3 +1,4 @@
+const http = require('http')
 const { join, resolve } = require('path')
 const esbuild = require('esbuild')
 const { glsl } = require('esbuild-plugin-glsl')
@@ -47,8 +48,44 @@ async function serve(path = defaultPath, port = 8000) {
     console.log(line)
   }
 
-  await esbuild.serve({ port, onRequest }, await getBuildOptions(path))
-  console.log(`Server listening at http://127.0.0.1:${port}`)
+  // await
+  esbuild
+    .serve({ port, onRequest }, await getBuildOptions(path))
+    .then((result) => {
+      // The result tells us where esbuild's local server is
+      const { host, port } = result
+
+      // Then start a proxy server on port 3000
+      http
+        .createServer((req, res) => {
+          const options = {
+            hostname: host,
+            port: port,
+            path: req.url,
+            method: req.method,
+            headers: req.headers,
+          }
+
+          // Forward each incoming request to esbuild
+          const proxyReq = http.request(options, (proxyRes) => {
+            // If esbuild returns "not found", send a custom 404 page
+            if (proxyRes.statusCode === 404) {
+              res.writeHead(404, { 'Content-Type': 'text/html' })
+              res.end('<h1>A custom 404 page</h1>')
+              return
+            }
+
+            // Otherwise, forward the response from esbuild to the client
+            res.writeHead(proxyRes.statusCode, proxyRes.headers)
+            proxyRes.pipe(res, { end: true })
+          })
+
+          // Forward the body of the request to esbuild
+          req.pipe(proxyReq, { end: true })
+        })
+        .listen(3000)
+    })
+  // console.log(`Server listening at http://127.0.0.1:${port}`)
 }
 
 let [a, b, command, path, option] = process.argv

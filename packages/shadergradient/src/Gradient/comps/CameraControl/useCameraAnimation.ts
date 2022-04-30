fc017c586aa04844d4cf7044b18e17d646c0bb79
ruucm <ruucm.a@gmail.com>
2022-04-30 21:22:16 +0900

    rorate the camera

--- packages/shadergradient/src/Gradient/comps/CameraControl/useCameraAnimation.ts
+++ packages/shadergradient/src/Gradient/comps/CameraControl/useCameraAnimation.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 import { useQueryState } from '../../../hooks/index'
 import { usePropertyStore } from '../../../store'
-import { useFiber } from '../../../utils/index'
+import { dToR, useFiber } from '../../../utils/index'
 
 const defaultDistance = 10
 const defaultZoom = 1
@@ -9,6 +9,7 @@ const defaultZoom = 1
 export function useCameraAnimation() {
   const { useFrame } = useFiber()
   const ref: any = useRef()
+  const control = ref.current
 
   useFrame((state, delta) => ref.current.update(delta)) // sync r3f delta with 'camera-controls'
 
@@ -25,27 +26,29 @@ export function useCameraAnimation() {
 
   // init the camera
   useEffect(() => {
-    const control = ref.current
-    control && control.dollyTo(cDistance, true)
-  }, [ref])
+    control?.dollyTo(cDistance, true)
+  }, [control])
+
+  // rorate the camera
+  useEffect(() => {
+    control?.rotateTo(dToR(cAzimuthAngle), dToR(cPolarAngle), true)
+  }, [control, cAzimuthAngle, cPolarAngle])
 
   // zoom-out tool
   useEffect(() => {
-    const control = ref.current
-
     if (zoomOut) {
-      control.dollyTo(defaultDistance, true)
-      control.zoomTo(defaultZoom, true)
+      control?.dollyTo(defaultDistance, true)
+      control?.zoomTo(defaultZoom, true)
     } else {
       if (type === 'sphere') {
-        control.zoomTo(cameraZoom, true)
-        control.dollyTo(defaultDistance, true)
+        control?.zoomTo(cameraZoom, true)
+        control?.dollyTo(defaultDistance, true)
       } else {
-        control.dollyTo(cDistance, true)
-        control.zoomTo(defaultZoom, true)
+        control?.dollyTo(cDistance, true)
+        control?.zoomTo(defaultZoom, true)
       }
     }
-  }, [ref, zoomOut, type])
+  }, [control, zoomOut, type])
 
   return ref
 }

8be8b6f4f8babe8c5522d7fdb2186ebfa89415a6
deploy <deploy@users.noreply.github.com>
2022-05-01 05:58:19 +0000

    Updated

---
+++ shadergradient@0.2.21/Gradient/comps/CameraControl/useCameraAnimation.js
@@ -0,0 +1,40 @@
+// src/Gradient/comps/CameraControl/useCameraAnimation.ts
+import { useEffect, useRef } from "react";
+import { defaultDistance, defaultZoom } from "../../../consts.js";
+import { useQueryState } from "../../../hooks/index.js";
+import { usePropertyStore } from "../../../store.js";
+import { dToR, useFiber } from "../../../utils/index.js";
+function useCameraAnimation() {
+  const { useFrame } = useFiber();
+  const ref = useRef();
+  const control = ref.current;
+  useFrame((state, delta) => ref.current.update(delta));
+  const [cAzimuthAngle] = useQueryState("cAzimuthAngle");
+  const [cPolarAngle] = useQueryState("cPolarAngle");
+  const [cDistance] = useQueryState("cDistance");
+  const [cameraZoom] = useQueryState("cameraZoom");
+  const hoverState = usePropertyStore((state) => state.hoverState);
+  const zoomOut = usePropertyStore((state) => state.zoomOut);
+  const [type] = useQueryState("type");
+  useEffect(() => {
+    control == null ? void 0 : control.rotateTo(dToR(cAzimuthAngle), dToR(cPolarAngle), true);
+  }, [control, cAzimuthAngle, cPolarAngle]);
+  useEffect(() => {
+    if (zoomOut) {
+      control == null ? void 0 : control.dollyTo(defaultDistance, true);
+      control == null ? void 0 : control.zoomTo(defaultZoom, true);
+    } else {
+      if (type === "sphere") {
+        control == null ? void 0 : control.zoomTo(cameraZoom, true);
+        control == null ? void 0 : control.dollyTo(defaultDistance, true);
+      } else {
+        control == null ? void 0 : control.dollyTo(cDistance, true);
+        control == null ? void 0 : control.zoomTo(defaultZoom, true);
+      }
+    }
+  }, [control, zoomOut, type]);
+  return ref;
+}
+export {
+  useCameraAnimation
+};

ac5204cf8e06d00677520fc6b19a83f1895764e4
deploy <deploy@users.noreply.github.com>
2022-05-07 08:13:21 +0000

    Updated

---
+++ shadergradient@0.2.27/UI/Controls/index.js
@@ -0,0 +1,33 @@
+// src/UI/Controls/index.ts
+import { TestControl } from "./TestControl.js";
+import { ShapeControls } from "./ShapeControls.js";
+import { ColorControls } from "./ColorControls.js";
+import { EffectControls } from "./EffectControls.js";
+import { ViewControls } from "./ViewControls.js";
+import { BackgroundControls } from "./BackgroundControls.js";
+import {
+  Tools,
+  ToolsBox,
+  IconButtons,
+  Feather,
+  ControlTabTitles,
+  ControlTypeTitle,
+  ControlTypeTitles
+} from "./Tools/index.js";
+import { AllControls } from "./AllControls/index.js";
+export {
+  AllControls,
+  BackgroundControls,
+  ColorControls,
+  ControlTabTitles,
+  ControlTypeTitle,
+  ControlTypeTitles,
+  EffectControls,
+  Feather,
+  IconButtons,
+  ShapeControls,
+  TestControl,
+  Tools,
+  ToolsBox,
+  ViewControls
+};
---
+++ shadergradient@0.2.27/UI/Header/Header.js
@@ -0,0 +1,52 @@
+// src/UI/Header/Header.tsx
+import React from "react";
+import { PRESETS } from "../../presets.js";
+import { useUIStore } from "../../store.js";
+import { AboutBtn } from "./AboutBtn.js";
+
+// esbuild-css-modules-plugin-namespace:/tmp/tmp-1909-EHPEFRIMFZj6/shadergradient/src/UI/Header/Header.module.css.js
+var digest = "9b32982b686a89bd95708372c3699d080f454c3b1f648c4273a8335d5571e9e8";
+var css = `._header_fvc3v_1 {
+  width: 100vw;
+  height: 8vh;
+  display: flex;
+  justify-content: center;
+  align-items: center; }
+`;
+(function() {
+  if (!document.getElementById(digest)) {
+    var ele = document.createElement("style");
+    ele.id = digest;
+    ele.textContent = css;
+    document.head.appendChild(ele);
+  }
+})();
+var Header_module_css_default = { "header": "_header_fvc3v_1" };
+
+// src/UI/Header/Header.tsx
+import { TextLogo } from "./TextLogo.js";
+function Header({
+  mode = "full",
+  isMobile = false,
+  inAbout = false,
+  aboutBtn = false,
+  onLogoClick = () => void 0,
+  onAboutClick = () => void 0
+}) {
+  const activePreset = useUIStore((state) => state.activePreset);
+  const color = mode !== "full" ? "#FF430A" : PRESETS[activePreset].color;
+  return /* @__PURE__ */ React.createElement("div", {
+    className: Header_module_css_default.header
+  }, /* @__PURE__ */ React.createElement(TextLogo, {
+    color,
+    size: isMobile === true ? 15 : 18,
+    onClick: onLogoClick
+  }), aboutBtn && /* @__PURE__ */ React.createElement(AboutBtn, {
+    inAbout,
+    color,
+    onClick: onAboutClick
+  }));
+}
+export {
+  Header
+};

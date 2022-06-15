cb9fc9f16ef9b1534ed34489b5a00e9618906c60
ruucm <ruucm.a@gmail.com>
2022-06-15 22:00:24 +0900

    undo onClick

--- packages/shadergradient/src/UI/Controls/Tools/ToolsBox.tsx
+++ packages/shadergradient/src/UI/Controls/Tools/ToolsBox.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import cx from 'classnames'
-import { usePropertyStore } from '../../../store'
+import { updateGradientState, usePropertyStore } from '../../../store'
 import { IconButtons } from './IconButtons'
 
 type ControlTypeTitlePropsT = {
@@ -33,7 +33,23 @@ export const ToolsBox: React.FC<ControlTypeTitlePropsT> = ({
         borderRadius: 40,
       }}
     >
-      <IconButtons icon='CornerUpLeft' content='undo' />
+      <IconButtons
+        icon='CornerUpLeft'
+        content='undo'
+        onClick={() => {
+          const prevUrls = window.history.state.prevUrls || []
+
+          if (prevUrls.length > 1) {
+            prevUrls.pop() // remove current url
+
+            const lastURL = new URL(prevUrls[prevUrls.length - 1]).search
+            updateGradientState(lastURL)
+
+            prevUrls.pop() // remove the updated url(lastURL)
+            window.history.pushState({ prevUrls }, document.title, '') // sync the prevUrls
+          } else alert('no history')
+        }}
+      />
       <IconButtons
         icon='Box'
         content='3d axis'

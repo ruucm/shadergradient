d644f3236af98011501391afca75ee967d927afc
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2022-07-30 20:43:14 +0800

    disable cursor on customize

--- packages/website/src/components/layout/dom.tsx
+++ packages/website/src/components/layout/dom.tsx
@@ -26,11 +26,14 @@ const Dom = ({ children }) => {
     useStore.setState({ dom: ref })
   }, [])
 
+  useEffect(() => {
+    console.log(asPath)
+  }, [asPath])
+
   return (
     <div
       className='absolute top-0 left-0 z-10 w-full h-full overflow-hidden dom'
       ref={ref}
-      // style={{ cursor: 'none' }}
     >
       {children}
 

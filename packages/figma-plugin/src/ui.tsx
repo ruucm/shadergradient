7ea560d6da1c08aaa716ec86c829c783033dfee5
ruucm <ruucm.a@gmail.com>
2022-09-18 22:58:11 +0900

    update figmaPluginSite url

--- packages/figma-plugin/src/ui.tsx
+++ packages/figma-plugin/src/ui.tsx
@@ -3,8 +3,7 @@ import useFramerMessage from 'framer-sites-figma-plugin'
 import * as ReactDOM from 'react-dom'
 import './ui.css'
 
-// const figmaPluginSite = 'https://www.shadergradient.com/figma-plugin'
-const figmaPluginSite = 'https://shadergradient.framer.website/figma-plugin'
+const figmaPluginSite = 'https://www.shadergradient.co/figma-plugin'
 
 const App = () => {
   useFramerMessage()

47ccbd0235c7b4881767fb6c3fd278c3932fc215
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2022-10-23 16:44:26 +0800

    new color picker

--- packages/shadergradient/src/UI/Inputs/ColorInput/ColorInput.tsx
+++ packages/shadergradient/src/UI/Inputs/ColorInput/ColorInput.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
 import { useEffect, useState } from 'react'
+import { hexToHsva, hsvaToHex } from '@uiw/color-convert'
+import ShadeSlider from '@uiw/react-color-shade-slider'
+import Wheel from '@uiw/react-color-wheel'
+import { motion } from 'framer-motion'
+
 import { Spacing } from '../../../UI/index'
+import './slider.css'
 
 type ColorInputPropsT = {
   label?: string
@@ -14,7 +20,7 @@ type ColorInputPropsT = {
 export const ColorInput = React.forwardRef<HTMLInputElement, ColorInputPropsT>(
   ({ label = '', defaultValue, setValue }: ColorInputPropsT, ref) => {
     const [sharedValue, setSharedValue] = useState<any>(defaultValue)
-
+    const [toggle, setToggle] = useState(false)
     useEffect(() => {
       setSharedValue(defaultValue)
     }, [defaultValue])
@@ -34,16 +40,68 @@ export const ColorInput = React.forwardRef<HTMLInputElement, ColorInputPropsT>(
         )}
         <div className='flex items-center gap-2 w-full'>
           <div
-            className='w-full h-input rounded'
+            className='w-full h-input rounded relative'
             style={{ background: sharedValue }}
+            onClick={() => {
+              setToggle(!toggle)
+            }}
           >
-            <input
-              type='color'
-              value={sharedValue}
-              onChange={(e) => setSharedValue(e.target.value)}
-              className='w-full h-full opacity-0 cursor-pointer'
-              ref={ref}
-            />
+            {toggle === true && (
+              <motion.div
+                style={{
+                  width: 'fit-content',
+                  height: 290,
+                  position: 'absolute',
+                  bottom: 0,
+                  left: 80,
+                  zIndex: 100,
+                  background: 'blue',
+                }}
+              >
+                <div
+                  style={{
+                    position: 'fixed',
+                    display: 'flex',
+                    width: 'fit-content',
+                    height: 'fit-content',
+                    background: 'white',
+                    padding: 24,
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: 16,
+                    borderRadius: 5,
+                    boxShadow: 'rgb(0 0 0 / 37%) 0px 1px 4px 0px',
+                  }}
+                >
+                  <Wheel
+                    color={sharedValue}
+                    onChange={(color) => {
+                      console.log(hexToHsva(sharedValue))
+                      setSharedValue(color.hex)
+                    }}
+                    // ref={ref}
+                    width={200}
+                    height={200}
+                  ></Wheel>
+                  <ShadeSlider
+                    width={200}
+                    style={{ display: 'flex', alignItems: 'center' }}
+                    hsva={hexToHsva(sharedValue)}
+                    onChange={(color) => {
+                      setSharedValue(
+                        hsvaToHex({
+                          h: hexToHsva(sharedValue).h,
+                          s: color.s,
+                          v: color.v,
+                          a: 1,
+                        })
+                      )
+                    }}
+                  />
+                </div>
+              </motion.div>
+            )}
           </div>
 
           <input

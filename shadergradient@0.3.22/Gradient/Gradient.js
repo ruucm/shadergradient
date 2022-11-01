30e0eff5744392880d24c7f6adb5a3797cd557dc
deploy <deploy@users.noreply.github.com>
2022-11-01 15:26:54 +0000

    Updated

--- shadergradient@0.3.22/Gradient/Gradient.js
+++ shadergradient@0.3.22/Gradient/Gradient.js
@@ -32,7 +32,6 @@ var __objRest = (source, exclude) => {
 
 // src/Gradient/Gradient.tsx
 import React, { Suspense, useEffect } from "react";
-import { Axis } from "../Axis.js";
 import { envBasePath } from "../consts.js";
 import { usePostProcessing, useQueryState } from "../hooks/index.js";
 import { PRESETS } from "../presets.js";
@@ -63,14 +62,13 @@ function GradientComp(_a) {
   const dawn = useRGBELoader("dawn.hdr", { path: envBasePath });
   const lobby = useRGBELoader("lobby.hdr", { path: envBasePath });
   const textures = { city, dawn, lobby };
-  const toggleAxis = usePropertyStore((state) => state.toggleAxis);
   return /* @__PURE__ */ React.createElement(React.Fragment, null, lightType === "env" && /* @__PURE__ */ React.createElement(EnvironmentMap, {
     background: true,
     map: textures[envPreset],
     loadingCallback: setLoadingPercentage
   }), lightType === "3d" && /* @__PURE__ */ React.createElement("ambientLight", {
     intensity: brightness || 1
-  }), toggleAxis && /* @__PURE__ */ React.createElement(Axis, null), /* @__PURE__ */ React.createElement(CameraControl, __spreadValues({
+  }), /* @__PURE__ */ React.createElement(CameraControl, __spreadValues({
     dampingFactor
   }, others)), /* @__PURE__ */ React.createElement(GradientMesh, __spreadProps(__spreadValues({}, others), {
     rotSpringOption,
--- shadergradient@0.3.22/UI/Controls/AllControls/AllControls.js
+++ shadergradient@0.3.22/UI/Controls/AllControls/AllControls.js
@@ -5,8 +5,8 @@ import { useUIStore, useCursorStore } from "../../../store.js";
 import { PropertyControls } from "../../PropertyControls.js";
 import { ToolsBox, ControlTypeTitles } from "../Tools/index.js";
 
-// esbuild-css-modules-plugin-namespace:/tmp/tmp-2014-VITWSAJBziL6/shadergradient/src/UI/Controls/AllControls/AllControls.module.css.js
-var digest = "93257413412429a9f3a047dcea5d94184a23ad1cb986bd753a5c7d4133b4dee0";
+// esbuild-css-modules-plugin-namespace:/tmp/tmp-1969-uU8cDOsuRKFH/shadergradient/src/UI/Controls/AllControls/AllControls.module.css.js
+var digest = "cbf0cdb73dcfa3d3136147eda4cc0b0fa0c8766113b1d43f6b16d95e903aad49";
 var css = `._controlMobile_1dm53_1 {
   width: 100vw;
   height: fit-content; }

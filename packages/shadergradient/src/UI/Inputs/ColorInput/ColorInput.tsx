c7d68e708e56176247271eb332c776f1e2711745
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2022-11-01 21:35:13 +0800

    new color input

--- packages/shadergradient/src/UI/Inputs/ColorInput/ColorInput.tsx
+++ packages/shadergradient/src/UI/Inputs/ColorInput/ColorInput.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { hexToHsva, hsvaToHex } from '@uiw/color-convert'
+import ShadeSlider from '@uiw/react-color-shade-slider'
+import Wheel from '@uiw/react-color-wheel'
+import { useOnClickOutside } from '../../../hooks/index'
+
 import { Spacing } from '../../../UI/index'
+import './slider.css'
 
 type ColorInputPropsT = {
   label?: string
@@ -14,13 +20,16 @@ type ColorInputPropsT = {
 export const ColorInput = React.forwardRef<HTMLInputElement, ColorInputPropsT>(
   ({ label = '', defaultValue, setValue }: ColorInputPropsT, ref) => {
     const [sharedValue, setSharedValue] = useState<any>(defaultValue)
-
+    const [toggle, setToggle] = useState(false)
     useEffect(() => {
       setSharedValue(defaultValue)
     }, [defaultValue])
     useEffect(() => {
       setValue(sharedValue)
     }, [sharedValue])
+
+    const tabRef = useRef(null)
+    useOnClickOutside(tabRef, () => setToggle(false))
 
     return (
       <div className='flex items-center'>
@@ -32,20 +41,71 @@ export const ColorInput = React.forwardRef<HTMLInputElement, ColorInputPropsT>(
             <Spacing className='w-2' />
           </>
         )}
-        <div className='flex items-center gap-2 w-full'>
+        <div className='flex items-center gap-2 w-full relative'>
           <div
             className='w-full h-input rounded'
             style={{ background: sharedValue }}
-          >
-            <input
-              type='color'
-              value={sharedValue}
-              onChange={(e) => setSharedValue(e.target.value)}
-              className='w-full h-full opacity-0 cursor-pointer'
-              ref={ref}
-            />
-          </div>
+            onClick={() => {
+              setToggle(!toggle)
+            }}
+          ></div>
 
+          {/* color control */}
+          <div
+            ref={tabRef}
+            style={{
+              width: 'fit-content',
+              height: 290,
+              position: 'absolute',
+              bottom: 10,
+              left: 80,
+              zIndex: 100,
+              background: 'blue',
+              display: toggle === true ? 'block' : 'none',
+            }}
+          >
+            <div
+              style={{
+                position: 'fixed',
+                display: 'flex',
+                width: 'fit-content',
+                height: 'fit-content',
+                background: 'white',
+                padding: 24,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                gap: 16,
+                borderRadius: 5,
+                boxShadow: 'rgb(0 0 0 / 37%) 0px 1px 4px 0px',
+              }}
+            >
+              <Wheel
+                color={sharedValue}
+                onChange={(color) => {
+                  console.log(hexToHsva(sharedValue))
+                  setSharedValue(color.hex)
+                }}
+                width={200}
+                height={200}
+              ></Wheel>
+              <ShadeSlider
+                width={200}
+                style={{ display: 'flex', alignItems: 'center' }}
+                hsva={hexToHsva(sharedValue)}
+                onChange={(color) => {
+                  setSharedValue(
+                    hsvaToHex({
+                      h: hexToHsva(sharedValue).h,
+                      s: color.s,
+                      v: color.v,
+                      a: 1,
+                    })
+                  )
+                }}
+              />
+            </div>
+          </div>
           <input
             type='text'
             value={sharedValue}

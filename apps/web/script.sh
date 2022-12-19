e3af046e5b1e3b982c89c316abfc610bdc0ab958
ruucm <ruucm.a@gmail.com>
2022-12-19 13:43:47 +0900

    fix

--- apps/web/script.sh
+++ apps/web/script.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" !== "pages"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" != "pages"  ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;

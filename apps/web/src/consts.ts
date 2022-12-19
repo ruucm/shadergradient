616eccc556540e4be3cef18e2d8bee1e620ae1d8
ruucm <ruucm.a@gmail.com>
2022-12-19 10:39:58 +0900

    log

--- apps/web/src/consts.ts
+++ apps/web/src/consts.ts
@@ -1,2 +1,3 @@
-export const siteDomain = 'https://shadergradient.co'
-export const isDev = process.env.NODE_ENV !== 'production'
+export const siteDomain = "https://shadergradient.co";
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+export const isDev = process.env.NODE_ENV !== "production";

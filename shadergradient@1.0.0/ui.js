99e67f81e7e75d6d899655890c56c7baa2457b74
deploy <deploy@users.noreply.github.com>
2022-12-20 17:36:13 +0000

    Updated

--- shadergradient@1.0.0/ui.js
+++ shadergradient@1.0.0/ui.js
@@ -1,17 +1,59 @@
+var __defProp = Object.defineProperty;
+var __defProps = Object.defineProperties;
+var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
+var __getOwnPropSymbols = Object.getOwnPropertySymbols;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __propIsEnum = Object.prototype.propertyIsEnumerable;
+var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+var __spreadValues = (a, b) => {
+  for (var prop in b ||= {})
+    if (__hasOwnProp.call(b, prop))
+      __defNormalProp(a, prop, b[prop]);
+  if (__getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(b)) {
+      if (__propIsEnum.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    }
+  return a;
+};
+var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
+var __objRest = (source, exclude) => {
+  var target = {};
+  for (var prop in source)
+    if (__hasOwnProp.call(source, prop) && exclude.indexOf(prop) < 0)
+      target[prop] = source[prop];
+  if (source != null && __getOwnPropSymbols)
+    for (var prop of __getOwnPropSymbols(source)) {
+      if (exclude.indexOf(prop) < 0 && __propIsEnum.call(source, prop))
+        target[prop] = source[prop];
+    }
+  return target;
+};
+
+// src/utils.ts
+var cx = (...classes) => classes.filter((a) => !!a).join(" ");
+
 // src/UI/Button.tsx
 import { jsx } from "react/jsx-runtime";
-var Button = () => {
+var ButtonKind = {
+  primary: "bg-primary text-white",
+  secondary: "ring-1 ring-primary text-primary"
+};
+var Button = (_a) => {
+  var _b = _a, {
+    kind = "primary",
+    children
+  } = _b, rest = __objRest(_b, [
+    "kind",
+    "children"
+  ]);
   return /* @__PURE__ */ jsx(
-    "div",
-    {
-      style: {
-        background: "blue",
-        color: "white",
-        padding: 20,
-        display: "flex"
-      },
-      children: "ESM button33"
-    }
+    "button",
+    __spreadProps(__spreadValues({
+      className: cx("h-button font-medium rounded w-full", ButtonKind[kind])
+    }, rest), {
+      children
+    })
   );
 };
 
@@ -26,7 +68,7 @@ var Card = () => {
         padding: 20
       },
       children: [
-        /* @__PURE__ */ jsx2(Button, {}),
+        /* @__PURE__ */ jsx2(Button, { children: "Button in the card" }),
         "CAARD"
       ]
     }

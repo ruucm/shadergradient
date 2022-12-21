658fbd925b9d1ce262dcdf024cdea51e6c803eab
ruucm <ruucm.a@gmail.com>
2022-12-21 10:35:15 +0900

    fmt

--- apps/web/src/components/dom/Layout.tsx
+++ apps/web/src/components/dom/Layout.tsx
@@ -1,17 +1,17 @@
-import { useRef, forwardRef } from "react";
-import { mergeRefs } from "react-merge-refs";
+import { useRef, forwardRef } from 'react'
+import { mergeRefs } from 'react-merge-refs'
 
 const Layout = forwardRef(({ children, ...props }: any, ref) => {
-  const localRef = useRef();
+  const localRef = useRef()
   return (
     <div
       ref={mergeRefs([ref, localRef])}
-      className="absolute top-0 left-0 z-10 w-screen h-screen overflow-hidden dom" // bg-zinc-900 text-gray-50
+      className='absolute top-0 left-0 z-10 w-screen h-screen overflow-hidden dom' // bg-zinc-900 text-gray-50
     >
       {children}
     </div>
-  );
-});
-Layout.displayName = "Layout";
+  )
+})
+Layout.displayName = 'Layout'
 
-export default Layout;
+export default Layout

d6744ee4eb2e297d5268ca58dfa58677a63116ae
stone-skipper <stone.skipper.ee@gmail.com>
2022-12-30 16:00:04 +0800

    capitalize font

--- apps/web/src/pages/home/home.tsx
+++ apps/web/src/pages/home/home.tsx
@@ -128,7 +128,7 @@ export default function Page(props) {
                 <TextHover
                   fontSize={isMobile === true ? 15 : 18}
                   color={PRESETS[activePreset].color}
-                  font={'inter'}
+                  font={'Inter'}
                   content={'Create yours →'}
                   delay={0}
                   border

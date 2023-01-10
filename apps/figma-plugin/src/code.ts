70f72f2c286f52cb3e53cf7084ebdd6d89267a0c
ruucm <ruucm.a@gmail.com>
2023-01-11 01:33:23 +0700

    figma-plugin (re-import / quick)

---
+++ apps/figma-plugin/src/code.ts
@@ -0,0 +1,42 @@
+// import framerOnMessage from 'framer-sites-figma-plugin/lib/api'
+
+figma.showUI(__html__, { width: 453, height: 832 })
+
+figma.ui.onmessage = (msg) => {
+  // framerOnMessage(msg)
+  const { type } = msg
+  console.log('msg type', type)
+  if (type === 'SNAPSHOT') {
+    Promise.all(
+      figma.currentPage.selection.map((selected) =>
+        replaceToNewImage(selected, msg.bytes)
+      )
+    ).then(() => {
+      console.log('complete')
+    })
+  }
+}
+
+async function invertPaint(paint, bytes) {
+  if (paint.type === 'IMAGE') {
+    const newPaint = JSON.parse(JSON.stringify(paint))
+    newPaint.imageHash = figma.createImage(bytes).hash
+    return newPaint
+  } else {
+    // overwrite other types of paint (eg. SOLID)
+    const newPaint: ImagePaint = {
+      type: 'IMAGE',
+      scaleMode: 'FILL', // or FIT
+      imageHash: figma.createImage(bytes).hash,
+    }
+    return newPaint
+  }
+}
+
+async function replaceToNewImage(node, bytes) {
+  const newFills = []
+  for (const paint of node.fills) {
+    newFills.push(await invertPaint(paint, bytes))
+  }
+  node.fills = newFills
+}

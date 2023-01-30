3a3a205493f33590b5d28c2ad646633438b2c5b3
ruucm <ruucm.a@gmail.com>
2023-01-30 17:45:32 +0700

    update usePresetToStore logic

--- packages/shadergradient/src/store/usePresetToStore.ts
+++ packages/shadergradient/src/store/usePresetToStore.ts
@@ -2,33 +2,37 @@ import { useEffect } from 'react'
 import { PRESETS } from './presets'
 import { useUIStore, updateGradientState } from './store'
 
-let pageLoaded = false
 let loadedPreset
 
 export function usePresetToStore() {
   // ----------------------------- Preset to Custom Material ---------------------------------
   const activePreset = useUIStore((state: any) => state.activePreset)
   useEffect(() => {
-    let gradientURL
+    console.log('usePresetToStore (useEffect)')
+    console.log('activePreset', activePreset)
 
-    const hasSearchParam = window.location.search?.includes('pixelDensity')
+    let gradientURL
+    const hasSearchParam = window.location.search?.includes('pixelDensity') // checking just window.location.search existing is not valid for the Framer Preview search (?target=preview-web)
+
     // CASE 1. use search params at the first load.
-    if (
-      !pageLoaded &&
-      hasSearchParam // checking just window.location.search existing is not valid for the Framer Preview search (?target=preview-web)
-    ) {
-      console.log('[CASE 1] usePresetToStore')
+    if (hasSearchParam) {
+      console.log(
+        '[CASE 1] usePresetToStore (use search params at the first load.)'
+      )
       gradientURL = window.location.search
     }
     // CASE 2. When activePreset changes by UI buttons
-    else if (loadedPreset !== activePreset) {
-      console.log('[CASE 2] usePresetToStore')
+    else if (
+      (activePreset === 0 && !gradientURL) || // initial case
+      loadedPreset !== activePreset
+    ) {
+      console.log(
+        '[CASE 2] usePresetToStore (When activePreset changes by UI buttons)'
+      )
       gradientURL = PRESETS[activePreset].url
       loadedPreset = activePreset
     }
 
     updateGradientState(gradientURL)
-
-    pageLoaded = true
   }, [activePreset])
 }

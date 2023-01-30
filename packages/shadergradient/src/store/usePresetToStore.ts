3609f2c1ce290c31904fd86657de59682cc0297f
ruucm <ruucm.a@gmail.com>
2023-01-30 17:52:26 +0700

    restore pageLoaded

--- packages/shadergradient/src/store/usePresetToStore.ts
+++ packages/shadergradient/src/store/usePresetToStore.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import { PRESETS } from './presets'
 import { useUIStore, updateGradientState } from './store'
 
+let pageLoaded = false
 let loadedPreset
 
 export function usePresetToStore() {
@@ -15,7 +16,7 @@ export function usePresetToStore() {
     const hasSearchParam = window.location.search?.includes('pixelDensity') // checking just window.location.search existing is not valid for the Framer Preview search (?target=preview-web)
 
     // CASE 1. use search params at the first load.
-    if (hasSearchParam) {
+    if (hasSearchParam && !pageLoaded) {
       console.log(
         '[CASE 1] usePresetToStore (use search params at the first load.)'
       )
@@ -34,5 +35,6 @@ export function usePresetToStore() {
     }
 
     updateGradientState(gradientURL)
+    pageLoaded = true
   }, [activePreset])
 }

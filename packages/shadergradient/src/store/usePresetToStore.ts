b64eb3023a114861b38c3005dd91dd71bb646cd0
ruucm <ruucm.a@gmail.com>
2023-01-30 17:08:33 +0700

    fix usePresetToStore

--- packages/shadergradient/src/store/usePresetToStore.ts
+++ packages/shadergradient/src/store/usePresetToStore.ts
@@ -3,20 +3,29 @@ import { PRESETS } from './presets'
 import { useUIStore, updateGradientState } from './store'
 
 let pageLoaded = false
+let loadedPreset
+
 export function usePresetToStore() {
   // ----------------------------- Preset to Custom Material ---------------------------------
   const activePreset = useUIStore((state: any) => state.activePreset)
   useEffect(() => {
     let gradientURL
 
+    const hasSearchParam = window.location.search?.includes('pixelDensity')
     // CASE 1. use search params at the first load.
     if (
       !pageLoaded &&
-      window.location.search?.includes('pixelDensity') // checking just window.location.search existing is not valid for the Framer Preview search (?target=preview-web)
-    )
+      hasSearchParam // checking just window.location.search existing is not valid for the Framer Preview search (?target=preview-web)
+    ) {
+      console.log('[CASE 1] usePresetToStore')
       gradientURL = window.location.search
+    }
     // CASE 2. When activePreset changes by UI buttons
-    else gradientURL = PRESETS[activePreset].url
+    else if (loadedPreset !== activePreset) {
+      console.log('[CASE 2] usePresetToStore')
+      gradientURL = PRESETS[activePreset].url
+      loadedPreset = activePreset
+    }
 
     updateGradientState(gradientURL)
 

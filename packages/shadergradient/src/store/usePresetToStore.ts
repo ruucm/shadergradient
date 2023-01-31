d8ceb87373d78957403821124ac58cbe971bc992
ruucm <ruucm.a@gmail.com>
2023-01-31 15:28:19 +0700

    fix

--- packages/shadergradient/src/store/usePresetToStore.ts
+++ packages/shadergradient/src/store/usePresetToStore.ts
@@ -6,12 +6,13 @@ let pageLoaded = false
 export function usePresetToStore() {
   // ----------------------------- Preset to Custom Material ---------------------------------
   const activePreset = useUIStore((state: any) => state.activePreset)
+  console.log('activePreset. ', activePreset)
   useEffect(() => {
     let gradientURL
 
     // CASE 1. use search params at the first load.
     if (
-      !pageLoaded &&
+      // !pageLoaded &&
       window.location.search?.includes('pixelDensity') // checking just window.location.search existing is not valid for the Framer Preview search (?target=preview-web)
     ) {
       console.log('[CASE 1] usePresetToStore (Site first loaded with params)')

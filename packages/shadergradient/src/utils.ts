7de6a8d3f3407a72ebe394dc67b892d162f1f76b
ruucm <ruucm.a@gmail.com>
2023-01-31 19:01:52 +0700

    update formatUrlString

--- packages/shadergradient/src/utils.ts
+++ packages/shadergradient/src/utils.ts
@@ -7,9 +7,11 @@ export function dToRArr(degrees) {
 }
 
 export function formatUrlString(urlString) {
+  // remove domains (only search params should be left)
   return urlString
     .replace('http://localhost:3001/customize', '')
     .replace('https://shadergradient.co/customize', '')
+    .replace('https://www.shadergradient.co/customize', '')
 }
 
 export const cx = (...classes) => classes.filter((a) => !!a).join(' ')

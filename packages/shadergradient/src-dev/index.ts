7770f3ce4c5c5072624b49c10ce02a6bf1cd1569
ruucm <ruucm.a@gmail.com>
2023-05-29 20:24:37 +0900

    restore index.ts

--- packages/shadergradient/src-dev/index.ts
+++ packages/shadergradient/src-dev/index.ts
@@ -1,26 +1,24 @@
-// export * from './Box/index'
+export * from './Box/index'
 
-// export * from './GradientCanvas'
-// export * from './Gradient'
+export * from './GradientCanvas'
+export * from './Gradient'
 
-// // TODO: separate controls as a package (to do we should remove store stuff in the Gradient package first)
-// export * from './controls'
+// TODO: separate controls as a package (to do we should remove store stuff in the Gradient package first)
+export * from './controls'
 
-// // to use inside the web app
-// export {
-//   useUIStore,
-//   useCursorStore,
-//   updateGradientState,
-//   PRESETS,
-// } from './store'
-// export {
-//   mainLoading,
-//   aboutPositions,
-//   aboutRotations,
-//   aboutAngles,
-// } from './consts'
-// export { cx } from './utils'
+// to use inside the web app
+export {
+  useUIStore,
+  useCursorStore,
+  updateGradientState,
+  PRESETS,
+} from './store'
+export {
+  mainLoading,
+  aboutPositions,
+  aboutRotations,
+  aboutAngles,
+} from './consts'
+export { cx } from './utils'
 
-// export * from './Overrides'
-
-export const hello = `I'm index.ts (src-dev)`
+export * from './Overrides'

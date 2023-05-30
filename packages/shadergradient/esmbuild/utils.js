5d17f681865ec4353756c625b32e14e90f03823f
ruucm <ruucm.a@gmail.com>
2023-05-30 12:49:01 +0900

    install node-fetch

--- packages/shadergradient/esmbuild/utils.js
+++ packages/shadergradient/esmbuild/utils.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+const fetch = require('node-fetch')
+
 exports.getDevIPs = async () => {
   const res = await fetch(process.env.DEV_IPS_URL)
   const text = await res.text()

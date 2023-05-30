ed43d86aa3e0a3d824d3aaff98ff717171dd3846
ruucm <ruucm.a@gmail.com>
2023-05-30 12:26:06 +0900

    refactor: esmbuild-render

--- packages/shadergradient/esmbuild/esmbuild-render.js
+++ packages/shadergradient/esmbuild/esmbuild-render.js
@@ -1,62 +1,51 @@
-const { join, resolve } = require('path')
+const { join } = require('path')
+const http = require('http')
+const url = require('url')
+const requestIp = require('request-ip')
+const { getDevIPs } = require('./utils')
 const esbuild = require('esbuild')
 const { glsl } = require('esbuild-plugin-glsl')
 const { cssPlugin } = require('./plugin.css')
 const { dtsPlugin } = require('./plugin.dts')
 
-const http = require('http')
-const requestIp = require('request-ip')
-const { getDevIPs } = require('./utils')
-
-const color = (n, v) => `\x1b[${n}m${v}\x1b[0m`
-// const defaultPath = join(process.cwd(), 'src')
-const defaultOutdir = join(process.cwd(), 'dist')
-
+// consts
 const devPath = join(process.cwd(), 'src-dev')
 const prodPath = join(process.cwd(), 'src')
 
-async function build(outdir = defaultOutdir) {
-  outdir = resolve(outdir)
-  await esbuild.build({
-    outdir,
-    ...(await getBuildOptions(prodPath)),
-  })
-  console.log(`Build done at ${outdir}`)
+const port = Number(process.env.PORT || 10000)
+const devPort = port + 1
+const prodPort = port + 2
+
+const servedir = join(process.cwd(), 'dist')
+function onRequest(info) {
+  const status = color(
+    info.status.toString().startsWith('2') ? 32 : 31,
+    info.status
+  )
+  const line = color(
+    37,
+    `${info.method} ${status} ${info.path} [${info.timeInMS}ms]`
+  )
+  console.log(line)
 }
 
-console.log('process.env.PORT.', process.env.PORT)
-const targetPort = Number(process.env.PORT || 10000)
-console.log('targetPort', targetPort)
-const devPort = targetPort + 1
-const prodPort = targetPort + 2
-console.log({ devPort, prodPort })
-
-async function serve(mode) {
-  console.log('mode - serve', mode)
-  function onRequest(info) {
-    const status = color(
-      info.status.toString().startsWith('2') ? 32 : 31,
-      info.status
-    )
-    const line = color(
-      37,
-      `${info.method} ${status} ${info.path} [${info.timeInMS}ms]`
-    )
-    console.log(line)
-  }
-
-  const devServeOpt = { port: devPort, onRequest, servedir: defaultOutdir }
-  console.log('devServeOpt', devServeOpt)
+async function main(mode) {
+  // serve built files locally
+  const devServeOpt = { port: devPort, onRequest, servedir }
   await esbuild.serve(devServeOpt, await getBuildOptions(devPath))
   await esbuild.serve(
-    { port: prodPort, onRequest, servedir: defaultOutdir },
+    { port: prodPort, onRequest, servedir },
     await getBuildOptions(prodPath)
   )
 
-  // Then start a conditional server on port targetPort
-  http
-    .createServer((req, res) => {
-      requestIp.mw()(req, res, async () => {
+  // start server
+  const server = http.createServer((req, res) => {
+    requestIp.mw()(req, res, async () => {
+      console.log('req.url', req.url)
+      let pathname = url.parse(req.url, true).pathname
+      console.log('pathname.', pathname)
+
+      if (pathname === '/debug') {
         const clientIp = req.clientIp
         console.log('clientIp', clientIp)
         const devIPs = await getDevIPs()
@@ -65,54 +54,73 @@ async function serve(mode) {
         const isDev = mode === 'devMode' || devIPs.includes(clientIp)
         console.log('isDev', isDev)
 
-        // Forward each incoming request to esbuild
-        const debugInfo = {
-          mode,
-          clientIp,
-          devIPs,
-          isDev,
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end(
+          `Your IP address is: ${clientIp} / mode: ${mode} / isDev: ${isDev}`
+        )
+        return
+      } else if (pathname === '/hello') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' })
+        res.end('hello')
+      } else {
+        // res.writeHead(404)
+        // res.end('not found')
+        const clientIp = req.clientIp
+        console.log('clientIp', clientIp)
+        const devIPs = await getDevIPs()
+        console.log('devIPs', devIPs)
+
+        const isDev = mode === 'devMode' || devIPs.includes(clientIp)
+        console.log('isDev', isDev)
+
+        // serve built files publically
+        const proxyOptions = {
+          hostname: '0.0.0.0',
+          port: isDev ? devPort : prodPort,
+          path: req.url,
+          method: req.method,
+          headers: req.headers,
         }
-        console.log('req.url', req.url)
-        if (req.url === '/debug') {
-          // Print debug information
-          res.writeHead(200, { 'Content-Type': 'text/html' })
-          res.end(`<pre>${JSON.stringify(debugInfo, null, 4)}</pre>`)
-          return
-        } else {
-          // conditional server
-          const proxyOptions = {
-            hostname: '0.0.0.0',
-            port: isDev ? devPort : prodPort,
-            path: req.url,
-            method: req.method,
-            headers: req.headers,
+        const proxyReq = http.request(proxyOptions, (proxyRes) => {
+          // If esbuild returns "not found", send a custom 404 page
+          if (proxyRes.statusCode === 404) {
+            res.writeHead(404, { 'Content-Type': 'text/html' })
+            res.end(`<h1>A custom 404 page</h1>`)
+            return
           }
-          const proxyReq = http.request(proxyOptions, (proxyRes) => {
-            // If esbuild returns "not found", send a custom 404 page
-            if (proxyRes.statusCode === 404) {
-              res.writeHead(404, { 'Content-Type': 'text/html' })
-              res.end(`<h1>A custom 404 page</h1>`)
-              return
-            }
 
-            // Otherwise, forward the response from esbuild to the client
-            res.writeHead(proxyRes.statusCode, proxyRes.headers)
-            proxyRes.pipe(res, { end: true })
-          })
+          // Otherwise, forward the response from esbuild to the client
+          res.writeHead(proxyRes.statusCode, proxyRes.headers)
+          proxyRes.pipe(res, { end: true })
+        })
 
-          // Forward the body of the request to esbuild
-          req.pipe(proxyReq, { end: true })
-        }
-      })
+        // Forward the body of the request to esbuild
+        req.pipe(proxyReq, { end: true })
+      }
     })
-    .listen(targetPort)
 
-  console.log(`Server listening at http://127.0.0.1:${targetPort}`)
+    // if (pathname === '/debug') {
+    //   res.writeHead(200, { 'Content-Type': 'text/plain' })
+    //   res.end(`some debug infos`)
+    // } else if (pathname === '/hello') {
+    //   res.writeHead(200, { 'Content-Type': 'text/plain' })
+    //   res.end('hello')
+    // } else if (pathname === '/') {
+    //   res.writeHead(200, { 'Content-Type': 'text/plain' })
+    //   res.end('home')
+    // } else {
+    //   res.writeHead(404)
+    //   res.end('not found')
+    // }
+  })
+
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
 }
 
 async function getBuildOptions(path) {
   const isDev = path === devPath
-  console.log('path', path)
   console.log('isDev - getBuildOptions', isDev)
 
   return {
@@ -142,14 +150,7 @@ async function getBuildOptions(path) {
   }
 }
 
-let [a, b, command, mode] = process.argv
+let [a, b, mode] = process.argv
 
 console.log('mode -  process.argv', mode)
-
-if (command === 'serve') {
-  serve(mode)
-} else if (command === 'build') {
-  build(mode)
-} else {
-  console.log(`Usage:\n  $ esbuild serve src 8000\n  $ esbuild build src dist`)
-}
+main(mode)

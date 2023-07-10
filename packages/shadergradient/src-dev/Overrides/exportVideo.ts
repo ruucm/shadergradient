933c001d4008732b0f1d84faa8fd349f72d1ea01
ruucm <ruucm.a@gmail.com>
2023-07-10 17:41:29 +0900

    update exportVideo

--- packages/shadergradient/src-dev/Overrides/exportVideo.ts
+++ packages/shadergradient/src-dev/Overrides/exportVideo.ts
@@ -1,45 +1,57 @@
 import { sleep } from './utils'
 
-export async function exportVideo(duration) {
-  const recordingTimeMS = duration * 1000
-  const preview: any = document.getElementById('gradientCanvas')
+export async function exportVideo(option, callback) {
+  const { rangeStart, rangeEnd } = option
+  const duration = rangeEnd - rangeStart // seconds
+  recordVideo(duration)
+
+  // setProgress
+  const interval = setInterval(() => {
+    callback((prev) => {
+      const progress = prev + 1 / duration
+      if (progress >= 1) clearInterval(interval)
+      return progress
+    })
+  }, 1000)
+}
+
+let mediaRecorder
+let recordedChunks = []
+
+export async function recordVideo(duration) {
+  const canvas: any = document.getElementById('gradientCanvas')
     ?.children[0] as HTMLCanvasElement
 
-  preview.captureStream = preview.captureStream || preview.mozCaptureStream
+  recordedChunks = []
+  const stream = canvas.captureStream()
+  mediaRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' })
 
-  startRecording(preview.captureStream(), recordingTimeMS).then(
-    (recordedChunks) => {
-      let recordedBlob = new Blob(recordedChunks, { type: 'video/webm' })
-      const videoUrl = URL.createObjectURL(recordedBlob)
-      const videoElement = document.createElement('video')
-      videoElement.controls = true
-      videoElement.src = videoUrl
-      document.body.appendChild(videoElement)
-
-      console.log(
-        `Successfully recorded ${recordedBlob.size} bytes of ${recordedBlob.type} media.`
-      )
+  mediaRecorder.addEventListener('dataavailable', (e) => {
+    if (e.data.size > 0) {
+      recordedChunks.push(e.data)
     }
-  )
+  })
+
+  mediaRecorder.addEventListener('stop', () => {
+    downloadVideo()
+  })
+
+  mediaRecorder.start()
+
+  await sleep(duration)
+  mediaRecorder.stop()
 }
-function startRecording(stream, lengthInMS) {
-  let recorder = new MediaRecorder(stream)
-  let data = []
 
-  recorder.ondataavailable = (event) => data.push(event.data)
-  recorder.start()
-  console.log(`${recorder.state} for ${lengthInMS / 1000} seconds…`)
-
-  let stopped = new Promise((resolve, reject) => {
-    recorder.onstop = resolve
-    recorder.onerror = (event: any) => reject(event.name)
+function downloadVideo() {
+  const blob = new Blob(recordedChunks, {
+    type: 'video/webm',
   })
-
-  let recorded = sleep(lengthInMS * 1000).then(() => {
-    if (recorder.state === 'recording') {
-      recorder.stop()
-    }
-  })
-
-  return Promise.all([stopped, recorded]).then(() => data)
+  const url = URL.createObjectURL(blob)
+  const a: any = document.createElement('a')
+  document.body.appendChild(a)
+  a.style = 'display: none'
+  a.href = url
+  a.download = 'test.webm'
+  a.click()
+  window.URL.revokeObjectURL(url)
 }

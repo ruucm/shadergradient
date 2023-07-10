fd888d9a79e9a6de112e16586bde7d864ec81fcf
ruucm <ruucm.a@gmail.com>
2023-07-10 17:41:37 +0900

    change video name

--- packages/shadergradient/src-dev/Overrides/exportVideo.ts
+++ packages/shadergradient/src-dev/Overrides/exportVideo.ts
@@ -51,7 +51,7 @@ function downloadVideo() {
   document.body.appendChild(a)
   a.style = 'display: none'
   a.href = url
-  a.download = 'test.webm'
+  a.download = 'shadergradient.webm'
   a.click()
   window.URL.revokeObjectURL(url)
 }

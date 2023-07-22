af30457347a87130daf0166edddb8fb20cce50fd
stone-skipper <stone.skipper.ee@gmail.com>
2023-07-22 14:13:00 +0800

    style fix

--- packages/shadergradient/src-dev/Controls/AllControls/PropertyControls.tsx
+++ packages/shadergradient/src-dev/Controls/AllControls/PropertyControls.tsx
@@ -29,14 +29,12 @@ export const PropertyControls: React.FC<Props> = ({
   return (
     <motion.div
       className={cx(
-        'w-full bg-controls-panel-mobile text-primary mx-auto md:h-full relative hide-scrollbar'
+        'w-full bg-controls-panel-mobile text-primary mx-auto relative hide-scrollbar h-fit'
       )}
       style={{
         originY: 1,
       }}
       animate={{
-        height: 'fit-content',
-        maxHeight: activeTab === 'none' ? 0 : 600,
         padding: activeTab === 'none' ? 0 : 20,
         overflow: activeTab === 'none' ? 'hidden' : 'visible',
         borderRadius: isMobile === true ? 0 : '4px 4px 0 0',
@@ -45,7 +43,6 @@ export const PropertyControls: React.FC<Props> = ({
       transition={{
         duration: 0.72,
         padding: { duration: 0 },
-        maxHeight: { duration: 0 },
         overflow: { delay: activeTab !== 'none' ? 0.72 : 0 },
       }}
     >

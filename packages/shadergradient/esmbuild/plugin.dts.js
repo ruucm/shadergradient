7ceae30e87c0abd91e6213c0b27f90d954741a4e
ruucm <ruucm.a@gmail.com>
2023-09-12 22:01:02 +0900

    log

--- packages/shadergradient/esmbuild/plugin.dts.js
+++ packages/shadergradient/esmbuild/plugin.dts.js
@@ -8,7 +8,10 @@ exports.dtsPlugin = () => {
       build.onEnd((result) => {
         if (result.errors.length > 0) return
         try {
-          execSync('ls')
+          const files = execSync('ls').toString()
+          console.log('[Files in the current directory]:')
+          console.log(files)
+
           execSync('tsc --emitDeclarationOnly --outDir dist')
         } catch (error) {
           console.log('[tsc error]', error)

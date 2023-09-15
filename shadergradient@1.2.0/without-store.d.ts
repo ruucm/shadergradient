57109ded5d85dbffc9e5b44a595172de384bc141
deploy <deploy@users.noreply.github.com>
2023-09-15 06:27:21 +0000

    Updated

--- shadergradient@1.2.0/without-store.d.ts
+++ shadergradient@1.2.0/without-store.d.ts
@@ -1,3 +1,3 @@
-export * from './GradientCanvasWithoutStore';
-export * from './Gradient/GradientWithoutStore';
+export * from './ShaderGradientCanvas';
+export * from './Gradient/ShaderGradient';
 //# sourceMappingURL=without-store.d.ts.map

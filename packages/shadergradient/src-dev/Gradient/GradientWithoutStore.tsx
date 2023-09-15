858814d4bba21fe351273710c17c6fe3916383ff
ruucm <ruucm.a@gmail.com>
2023-09-15 14:37:16 +0900

    support query control (GradientWithoutStore)

--- packages/shadergradient/src-dev/Gradient/GradientWithoutStore.tsx
+++ packages/shadergradient/src-dev/Gradient/GradientWithoutStore.tsx
@@ -1,6 +1,8 @@
 import { usePostProcessing } from './hooks/usePostProcessing'
 import { Lights, Mesh, Axis, CameraControl } from './comps-without-store'
 import { GradientT } from '../types'
+import * as qs from 'query-string'
+import { formatUrlString } from '@/utils'
 
 export function GradientWithoutStore({
   control = 'props',
@@ -10,8 +12,16 @@ export function GradientWithoutStore({
   isFigmaPlugin = false,
   ...props
 }: GradientT) {
+  let controlProps = props
+  if (control === 'query')
+    controlProps = qs.parse(formatUrlString(props.urlString), {
+      parseNumbers: true,
+      parseBooleans: true,
+      arrayFormat: 'index',
+    })
   const { lightType, envPreset, brightness, grain, toggleAxis, ...others } =
-    props
+    controlProps
+
   usePostProcessing(grain === 'off')
 
   return (

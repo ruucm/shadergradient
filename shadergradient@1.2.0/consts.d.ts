74215baafab2b3e40b10866242afbfd83fb1b002
deploy <deploy@users.noreply.github.com>
2023-10-01 20:47:47 +0000

    Updated

--- shadergradient@1.2.0/consts.d.ts
+++ shadergradient@1.2.0/consts.d.ts
@@ -40,7 +40,7 @@ export declare const mainLoading: {
     to: number;
 };
 export declare const envBasePath = "https://ruucm.github.io/shadergradient/shadergradient@1.0.0/hdr/";
-export declare const STRIPE_BUY_YEARLY_URL = "https://buy.stripe.com/eVa7uocSxgMnaZi5kl";
-export declare const STRIPE_BUY_MONTHLY_URL = "https://buy.stripe.com/28o01W05LeEfgjC5km";
-export declare const STRIPE_BILLING_URL = "https://billing.stripe.com/p/login/cN27vf1dL52j5c4cMM";
+export declare const STRIPE_BUY_YEARLY_URL = "https://buy.stripe.com/00g5kV0g553QcjC9AB";
+export declare const STRIPE_BUY_MONTHLY_URL = "https://buy.stripe.com/fZe5kVd2R53QcjC7su";
+export declare const STRIPE_BILLING_URL = "https://billing.stripe.com/p/login/aEU8Audql6LagDe9AA";
 //# sourceMappingURL=consts.d.ts.map

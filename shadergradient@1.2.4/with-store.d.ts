5db6db840748bb10dd5b26bc348e6bee19075419
deploy <deploy@users.noreply.github.com>
2023-10-10 20:50:13 +0000

    Updated

--- shadergradient@1.2.4/with-store.d.ts
+++ shadergradient@1.2.4/with-store.d.ts
@@ -1,5 +1,6 @@
 export * from './StoreGradientCanvas';
 export * from './Gradient/StoreGradient';
+export * from './Gradient/Background';
 export * from './controls';
 export { useUIStore, useCursorStore, updateGradientState, PRESETS, } from './store';
 export { mainLoading, aboutPositions, aboutRotations, aboutAngles, } from './consts';

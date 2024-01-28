18ea0aa9e37217e75b4a9182219bdcd3ecd4b90e
ruucm <ruucm.a@gmail.com>
2024-01-27 19:33:38 -0800

    export Placeholder

--- packages/shadergradient/src-dev/ShaderGradientCanvas.tsx
+++ packages/shadergradient/src-dev/ShaderGradientCanvas.tsx
@@ -1,6 +1,7 @@
 import { Canvas } from '@react-three/fiber'
 import { canvasProps } from './consts'
 import { useContextLostFallback } from './useContextLostFallback'
+import { Placeholder } from './Placeholder'
 
 export function ShaderGradientCanvas({
   children,
@@ -31,32 +32,5 @@ export function ShaderGradientCanvas({
         {children}
       </Canvas>
     </>
-  )
-}
-
-export function Placeholder({
-  title = '',
-  color1 = '#ff7e5f',
-  color2 = '#feb47b',
-}) {
-  return (
-    <div
-      style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: `linear-gradient(to right, ${color1}, ${color2})`,
-        color: 'white',
-        fontSize: '20px',
-        textAlign: 'center',
-        padding: '20px',
-        fontFamily: 'Inter',
-        fontWeight: '500',
-      }}
-    >
-      <p>{title}</p>
-    </div>
   )
 }

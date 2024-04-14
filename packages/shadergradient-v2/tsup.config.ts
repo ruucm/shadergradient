ed1eaffd5dd1d6c64fc9aa962d150def5c87c69a
ruucm <ruucm.a@gmail.com>
2024-04-13 22:09:39 -0700

    serve es-modules

--- packages/shadergradient-v2/tsup.config.ts
+++ packages/shadergradient-v2/tsup.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'tsup'
 import { glsl } from 'esbuild-plugin-glsl'
+import http from 'http'
+import fs from 'fs'
+import path from 'path'
 
 const plugin = glsl({ minify: true })
 
@@ -15,8 +18,55 @@ export default defineConfig((options): any => {
       '@react-three/drei',
       '@react-three/fiber',
       'three',
-      'three-stdlib',
     ],
     esbuildPlugins: [plugin],
+    async onSuccess() {
+      // Create the HTTP server
+      const server = http.createServer((req, res) => {
+        // Construct the file path
+        let filePath = path.join(
+          __dirname,
+          'dist',
+          req.url === '/' ? 'index.html' : req.url
+        )
+        let extname = String(path.extname(filePath)).toLowerCase()
+
+        // Map file extensions to Content-Type
+        const mimeTypes = {
+          '.js': 'text/javascript',
+          '.mjs': 'application/javascript',
+          '.css': 'text/css',
+          '.html': 'text/html',
+        }
+
+        let contentType = mimeTypes[extname] || 'application/octet-stream'
+
+        // Set CORS headers
+        res.setHeader('Access-Control-Allow-Origin', '*') // Adjust as necessary for security
+
+        // Check if the file exists and serve it, otherwise serve a 404 page
+        fs.readFile(filePath, (error, content) => {
+          if (error) {
+            if (error.code === 'ENOENT') {
+              // File not found
+              res.writeHead(404, { 'Content-Type': 'text/html' })
+              res.end('<h1>404 Not Found</h1>', 'utf-8')
+            } else {
+              // Some server error
+              res.writeHead(500)
+              res.end(`Server error: ${error.code}`, 'utf-8')
+            }
+          } else {
+            // If no error, serve the file
+            res.writeHead(200, { 'Content-Type': contentType })
+            res.end(content, 'utf-8')
+          }
+        })
+      })
+      server.listen(8000)
+      return () => {
+        server.close()
+      }
+    },
   }
 })

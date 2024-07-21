96157c6d9eea007d973653f91fa8b77d048691be
deploy <deploy@users.noreply.github.com>
2024-07-21 03:09:17 +0000

    Updated

--- ui@0.0.0/index.d.ts
+++ ui@0.0.0/index.d.ts
@@ -1,8 +1,19 @@
 import * as React from 'react';
+import * as react_jsx_runtime from 'react/jsx-runtime';
 
 type ButtonPropsT = {
     kind?: 'primary' | 'secondary';
 } & React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>;
 declare const Button: React.FC<ButtonPropsT>;
 
-export { Button };
+declare function TextAnimation({ fontSize, color, content, delay, width, yBefore, isFramerCanvas, }: {
+    fontSize: any;
+    color: any;
+    content: any;
+    delay: any;
+    width?: any;
+    yBefore?: number;
+    isFramerCanvas?: boolean;
+}): react_jsx_runtime.JSX.Element;
+
+export { Button, TextAnimation };

9944abf5c0895a8f6cba80b34aa28ad98486c1f5
ruucm <ruucm.a@gmail.com>
2024-09-15 23:07:20 +0900

    refactors

--- packages/shadergradient-v2/src/Plane/Mesh.tsx
+++ packages/shadergradient-v2/src/Plane/Mesh.tsx
@@ -1,21 +1,34 @@
 import React, { useRef } from 'react'
 import { vertexShader, fragmentShader } from './shader'
+import { useFrame } from '@react-three/fiber'
+import { degToRad } from '@/utils'
+import * as THREE from 'three'
 
-const amplitude = 0.9
+const amplitude = 2
 
 export function Mesh({ width, height }) {
   const materialRef = useRef()
 
+  useFrame(({ clock }) => {
+    const elapsedTime = clock.getElapsedTime()
+    if (materialRef.current) {
+      materialRef.current.uniforms.u_time.value = elapsedTime
+      materialRef.current.uniforms.u_amplitude.value = amplitude
+    }
+  })
+
   return (
-    <mesh>
+    <mesh rotation={[degToRad(0), degToRad(0), degToRad(0)]}>
       <planeGeometry args={[width, height, 1, 1]} />
       <shaderMaterial
         ref={materialRef}
         attach='material'
         vertexShader={vertexShader}
         fragmentShader={fragmentShader}
+        side={THREE.DoubleSide}
+        // wireframe={true}
         uniforms={{
-          u_time: { value: 0.7 },
+          u_time: { value: 0.0 },
           u_amplitude: { value: amplitude },
           u_width: { value: width },
           u_height: { value: height },
--- packages/shadergradient-v2/src/Plane/shader/fragment.glsl
+++ packages/shadergradient-v2/src/Plane/shader/fragment.glsl
@@ -1,14 +1,14 @@
 varying vec3 fragPos;
 uniform float u_width;
 uniform float u_height;
+uniform float u_amplitude;
 
 void main() {
-    // vec4(r, g, b, a)
     vec3 color = fragPos;
 
     float x = clamp(fragPos.x / u_width, 0.0, 1.0);
     float y = clamp(fragPos.y / u_height, 0.0, 1.0);
-    float z = clamp(fragPos.z, 0.0, 1.0);
+    float z = clamp(fragPos.z / u_amplitude, 0.0, 1.0);
 
-    gl_FragColor = vec4(x, y, z, 1.0);
+    gl_FragColor = vec4(x, y, z, 1.0); // vec4(r, g, b, a)
 }

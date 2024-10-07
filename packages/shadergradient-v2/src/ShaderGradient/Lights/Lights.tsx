18a1834af142614e2670f430b3db2a990697f9e7
ruucm <ruucm.a@gmail.com>
2024-10-07 13:59:32 +0900

    comment

--- packages/shadergradient-v2/src/ShaderGradient/Lights/Lights.tsx
+++ packages/shadergradient-v2/src/ShaderGradient/Lights/Lights.tsx
@@ -1,6 +1,8 @@
 export function Lights(): JSX.Element {
   return (
     <>
+      {/* TODO: support higher version of three 0.154.0 */}
+      {/* it has darker reflection with the existing shader (packages/shadergradient-v2/src/shaders/a/fragment.glsl) */}
       <ambientLight intensity={1} />
     </>
   )

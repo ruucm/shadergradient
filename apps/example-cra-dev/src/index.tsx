2b54a8dc8148b365bb25e588343e88dea7ff3661
anonymousrecords <a.Records2022@gmail.com>
2024-10-30 15:57:00 +0900

    chore: delete unused files

--- apps/example-cra-dev/src/index.tsx
+++ apps/example-cra-dev/src/index.tsx
@@ -6,14 +6,9 @@ import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  // @ts-ignore
   <React.StrictMode>
-    {/* @ts-ignore */}
     <App />
   </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()

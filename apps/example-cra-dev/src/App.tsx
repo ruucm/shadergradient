5bcb6e9d6b2bf22c6fdf94d93a7b52d7f487d438
anonymousrecords <a.Records2022@gmail.com>
2024-10-30 15:57:20 +0900

    feat: update shadergradient

--- apps/example-cra-dev/src/App.tsx
+++ apps/example-cra-dev/src/App.tsx
@@ -1,27 +1,16 @@
-import { ShaderGradientCanvas, ShaderGradient } from '@shadergradient/react'
-import logo from './logo.svg'
-import './App.css'
+import { ShaderGradient, ShaderGradientCanvas } from '@shadergradient/react'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <ShaderGradientCanvas>
-          <ShaderGradient type='plane' />
-        </ShaderGradientCanvas>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ShaderGradientCanvas>
+        <ShaderGradient
+          cDistance={24}
+          color1='#ff5005'
+          color2='#dbba95'
+          color3='#d0bce1'
+        />
+      </ShaderGradientCanvas>
     </div>
   )
 }

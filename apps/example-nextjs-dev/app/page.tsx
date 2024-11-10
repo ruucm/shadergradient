f588006cf0bd832ab3ff577281749c2496068bf1
ruucm <ruucm.a@gmail.com>
2024-11-10 16:34:27 +0900

    update example

--- apps/example-nextjs-dev/app/page.tsx
+++ apps/example-nextjs-dev/app/page.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { ShaderGradientCanvas, ShaderGradient } from '@shadergradient/react'
+import { useState } from 'react'
 
 export default function Home() {
+  const [color1, setColor1] = useState('#0D77E0')
+
   return (
     <div className='flex'>
       <div className='flex-1 h-screen'>
@@ -18,8 +21,14 @@ export default function Home() {
       </div>
       <div className='flex-1 h-screen'>
         <ShaderGradientCanvas>
-          <ShaderGradient type='plane' color1='#0D77E0' grain='off' />
+          <ShaderGradient type='plane' color1={color1} grain='off' />
         </ShaderGradientCanvas>
+        <button
+          onClick={() => setColor1('#FF5005')}
+          className='fixed bottom-5 right-5 z-20'
+        >
+          Change Color
+        </button>
       </div>
     </div>
   )

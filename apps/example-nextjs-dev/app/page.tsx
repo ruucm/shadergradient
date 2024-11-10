b0799e11ed3f3b6e72ae8c37dfdfee7b4a56596f
ruucm <ruucm.a@gmail.com>
2024-11-10 16:21:55 +0900

    update example

--- apps/example-nextjs-dev/app/page.tsx
+++ apps/example-nextjs-dev/app/page.tsx
@@ -4,17 +4,23 @@ import { ShaderGradientCanvas, ShaderGradient } from '@shadergradient/react'
 
 export default function Home() {
   return (
-    <>
-      <div className='w-screen h-[50vh]'>
+    <div className='flex'>
+      <div className='flex-1 h-screen'>
         <ShaderGradientCanvas>
-          <ShaderGradient />
+          <ShaderGradient
+            cAzimuthAngle={0}
+            cPolarAngle={0}
+            rotationX={0}
+            rotationY={0}
+            rotationZ={0}
+          />
         </ShaderGradientCanvas>
       </div>
-      <div className='w-screen h-[50vh]'>
+      <div className='flex-1 h-screen'>
         <ShaderGradientCanvas>
           <ShaderGradient type='plane' color1='#0D77E0' grain='off' />
         </ShaderGradientCanvas>
       </div>
-    </>
+    </div>
   )
 }

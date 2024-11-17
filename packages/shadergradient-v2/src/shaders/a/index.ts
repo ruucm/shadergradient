8a1ff4fc0b1292cfaf478baaeb65ab09ea89336a
ruucm <ruucm.a@gmail.com>
2024-11-17 16:46:03 +0900

    fragmentSphere

--- packages/shadergradient-v2/src/shaders/a/index.ts
+++ packages/shadergradient-v2/src/shaders/a/index.ts
@@ -2,5 +2,6 @@ import vertexShader from './vertex.glsl'
 import fragmentShader from './fragment.glsl'
 
 import vertexSphere from './vertexSphere.glsl'
+import fragmentSphere from './fragmentSphere.glsl'
 
-export { vertexShader, fragmentShader, vertexSphere }
+export { vertexShader, fragmentShader, vertexSphere, fragmentSphere }

e6639cc229b571c373f15cf5f5fee1c22526296c
stone-skipper <stone.skipper.ee@gmail.com>
2024-11-17 15:46:39 +0800

    replacing the url origin

--- apps/framer-plugin/src/App.tsx
+++ apps/framer-plugin/src/App.tsx
@@ -8,16 +8,21 @@ framer.showUI({
   height: 446,
 })
 
-const framerPluginURLOrigin = 'https://delicious-statuses-566068.framer.app'
+const framerPluginURLOrigin = 'https://amicable-interface-702205.framer.app' // shadergradient.co origin
 
 const handleAddShaderGradient = async (controls: { message: string }) => {
   await framer.addComponentInstance({
-    url: 'https://framer.com/m/ShaderGradientStaging-8Yhr.js', //staging
-    // url: 'https://framer.com/m/ShaderGradient-oWuS.js', // upgraded shadergradient
+    // url: 'https://framer.com/m/ShaderGradientStaging-8Yhr.js', //staging
+    url: 'https://framer.com/m/ShaderGradient-oWuS.js', // upgraded shadergradient
     attributes: {
       width: '900px',
       height: '600px',
-      controls: controls,
+      controls: {
+        ...controls,
+        zoomOut: false,
+        toggleAxis: false,
+        range: 'disabled',
+      },
     },
   })
 }
@@ -38,19 +43,17 @@ export function App() {
 
   return (
     <main>
-      <div>
-        <iframe
-          id='framer-plugin-site'
-          src={framerPluginURLOrigin + '/framer-plugin'}
-          style={{
-            outline: 'none',
-            border: 'none',
-            width: 300,
-            height: 446,
-            overflow: 'hidden',
-          }}
-        />
-      </div>
+      <iframe
+        id='framer-plugin-site'
+        src={framerPluginURLOrigin + '/framer-plugin'}
+        style={{
+          outline: 'none',
+          border: 'none',
+          width: 300,
+          height: 446,
+          overflow: 'hidden',
+        }}
+      />
     </main>
   )
 }

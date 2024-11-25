dfeb62de8340b3bcdcc7d83e7b98b6ba1e2c6a67
ruucm <ruucm.a@gmail.com>
2024-11-25 11:21:41 +0900

    fix tsup config
    add src/ShaderGradientStateless/index.ts to entry

--- packages/shadergradient-v2/tsup.config.ts
+++ packages/shadergradient-v2/tsup.config.ts
@@ -43,7 +43,7 @@ export default defineConfig(async (options) => {
     platform: 'browser',
     format: ['esm'],
     dts: {
-      entry: 'src/index.ts',
+      entry: ['src/index.ts', 'src/ShaderGradientStateless/index.ts'],
     },
     minify: !isDev,
     clean: true,

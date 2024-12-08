e4557b6a4fe2a5c4d85f1d0e1e11acad738b3cfd
ruucm <ruucm.a@gmail.com>
2024-12-08 16:13:41 +0900

    export updateGradientState

--- packages/shadergradient-v2/src/ShaderGradientStateless/index.ts
+++ packages/shadergradient-v2/src/ShaderGradientStateless/index.ts
@@ -1,3 +1,4 @@
 export * from './store/useQueryState'
+export { updateGradientState } from './store/store'
 export * from './ShaderGradientStateless'
 export * from './ShaderGradientStatelessCanvas'

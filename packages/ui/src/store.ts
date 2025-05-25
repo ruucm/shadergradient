00aecfc4ba5c1eb146d58de1550e7e6443a09be1
seungmee-figma <seungmee.lee@figma.com>
2025-05-25 13:05:13 +0100

    adding store and changing to new url on figma plugin

--- packages/ui/src/store.ts
+++ packages/ui/src/store.ts
@@ -9,3 +9,29 @@ export const useUIStore = create(
       set((state) => ({ ...state, loadingPercentage: data })),
   }))
 )
+
+
+const useFigmaStore = create((set) => ({
+  figma: { selection: 0, user: null },
+  setFigma: (payload) =>
+    set((prev) => ({ figma: { ...prev.figma, ...payload } })),
+}))
+export function useFigma() {
+  const figma = useFigmaStore((state: any) => state.figma)
+  const setFigma = useFigmaStore((state: any) => state.setFigma)
+  return [figma, setFigma]
+}
+export const useBillingIntervalStore = create((set) => ({
+  billingInterval: 'year',
+  setBillingInterval: (payload) =>
+    set((state) => ({ billingInterval: payload })),
+}))
+export function useBillingInterval() {
+  const billingInterval = useBillingIntervalStore(
+    (state: any) => state.billingInterval
+  )
+  const setBillingInterval = useBillingIntervalStore(
+    (state: any) => state.setBillingInterval
+  )
+  return [billingInterval, setBillingInterval]
+}

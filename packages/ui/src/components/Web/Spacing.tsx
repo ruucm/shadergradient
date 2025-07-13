40ce31a65eb83ba6372b45ee80c5f0dafce46aaa
ruucm <ruucm.a@gmail.com>
2025-07-13 23:29:49 +0900

    components/Web/Customize

---
+++ packages/ui/src/components/Web/Spacing.tsx
@@ -0,0 +1,25 @@
+export const Spacing = ({ guide = false, className = 'w-3 h-3', ...props }) => (
+  <div
+    {...props}
+    className={className}
+    style={{
+      position: 'relative',
+
+      background: 'hsl(100, 37%, 79%)',
+      opacity: 'var(--guide-opacity)',
+    }}
+  >
+    <div
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        whiteSpace: 'nowrap',
+        fontSize: '8px',
+      }}
+    >
+      {guide && className}
+    </div>
+  </div>
+)

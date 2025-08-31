e7225a0182161dac106943639586fdc8b9d6418d
ruucm <ruucm.a@gmail.com>
2025-09-01 01:53:51 +0900

    fix: ShaderGradientStatelessCanvas (fov, pixelDensity)

--- packages/ui/src/components/Shared/ShaderGradientStateless/ShaderGradientStatelessCanvas.tsx
+++ packages/ui/src/components/Shared/ShaderGradientStateless/ShaderGradientStatelessCanvas.tsx
@@ -1,10 +1,15 @@
 import { ShaderGradientCanvas } from '@shadergradient/react'
 import { useQueryState } from './store/useQueryState'
-import { canvasProps } from '@/utils'
 
 export function ShaderGradientStatelessCanvas(props) {
   const [pixelDensity] = useQueryState('pixelDensity')
   const [fov] = useQueryState('fov')
 
-  return <ShaderGradientCanvas {...props} {...canvasProps(pixelDensity, fov)} />
+  return (
+    <ShaderGradientCanvas
+      {...props}
+      pixelDensity={pixelDensity || 1}
+      fov={fov || 45}
+    />
+  )
 }

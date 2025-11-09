360622d7f2ce94a95330de1cc660bfbc41f1d591
deploy <deploy@users.noreply.github.com>
2025-11-09 15:57:21 +0000

    Updated

---
+++ ui@0.1.30/overrides/FigmaPlugin/exportGIF.mjs
@@ -0,0 +1,18 @@
+import {
+  exportGIF
+} from "../../chunk-ITIMCFVA.mjs";
+import "../../chunk-NAZGFUZO.mjs";
+import "../../chunk-CK47F4LN.mjs";
+import "../../chunk-DC3YOL4Q.mjs";
+import "../../chunk-BI4IT72G.mjs";
+import "../../chunk-XKMCKCFY.mjs";
+import "../../chunk-2WMPBKSG.mjs";
+import "../../chunk-64EKMGYT.mjs";
+import "../../chunk-BGA6FIQU.mjs";
+import "../../chunk-64AEIACY.mjs";
+import "../../chunk-BJ4NI3NE.mjs";
+import "../../chunk-NP5XZW6Z.mjs";
+import "../../chunk-HXGKXP63.mjs";
+export {
+  exportGIF
+};
---
+++ ui@0.1.30/overrides/Web/Scroll.mjs
@@ -0,0 +1,30 @@
+import {
+  CenterCrop1,
+  CenterCrop2,
+  Gap,
+  SectionObserver,
+  SectionText,
+  WordGradient,
+  WordGradientText,
+  WordHighlight,
+  WordHighlight2,
+  fpsCount,
+  wordHighlightText
+} from "../../chunk-OV6DHU6Z.mjs";
+import "../../chunk-425BLYMY.mjs";
+import "../../chunk-BJ4NI3NE.mjs";
+import "../../chunk-NP5XZW6Z.mjs";
+import "../../chunk-HXGKXP63.mjs";
+export {
+  CenterCrop1,
+  CenterCrop2,
+  Gap,
+  SectionObserver,
+  SectionText,
+  WordGradient,
+  WordGradientText,
+  WordHighlight,
+  WordHighlight2,
+  fpsCount,
+  wordHighlightText
+};

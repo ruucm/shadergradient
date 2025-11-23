631a07b674078c42d7bca477d6ea791d818131c1
deploy <deploy@users.noreply.github.com>
2025-11-23 09:53:55 +0000

    Updated

---
+++ ui@0.1.31/components/Web/ScrollableBox.mjs
@@ -0,0 +1,11 @@
+"use client";
+import {
+  ScrollableTextBox
+} from "../../chunk-GM76PT2V.mjs";
+import "../../chunk-ZRY2JHRC.mjs";
+import "../../chunk-OOWSMYW2.mjs";
+import "../../chunk-NP5XZW6Z.mjs";
+import "../../chunk-HXGKXP63.mjs";
+export {
+  ScrollableTextBox
+};

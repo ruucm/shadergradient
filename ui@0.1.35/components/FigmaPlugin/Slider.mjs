dce191dd60b9121f728d46b8b1978a692c71ae8c
deploy <deploy@users.noreply.github.com>
2025-11-23 15:57:48 +0000

    Updated

---
+++ ui@0.1.35/components/FigmaPlugin/Slider.mjs
@@ -0,0 +1,9 @@
+import {
+  Slider
+} from "../../chunk-R6AEDQIR.mjs";
+import "../../chunk-2FOTMNF6.mjs";
+import "../../chunk-SIKJUSOK.mjs";
+import "../../chunk-HXGKXP63.mjs";
+export {
+  Slider
+};
---
+++ ui@0.1.35/overrides/Web/Scroll.mjs
@@ -0,0 +1,30 @@
+import {
+  CenterCrop1,
+  CenterCrop2,
+  Gap,
+  SectionObserver,
+  SectionText,
+  WordGradient,
+  WordGradientText,
+  WordHighlight,
+  WordHighlight2,
+  fpsCount,
+  wordHighlightText
+} from "../../chunk-ZG4T2OTY.mjs";
+import "../../chunk-425BLYMY.mjs";
+import "../../chunk-OOWSMYW2.mjs";
+import "../../chunk-NP5XZW6Z.mjs";
+import "../../chunk-HXGKXP63.mjs";
+export {
+  CenterCrop1,
+  CenterCrop2,
+  Gap,
+  SectionObserver,
+  SectionText,
+  WordGradient,
+  WordGradientText,
+  WordHighlight,
+  WordHighlight2,
+  fpsCount,
+  wordHighlightText
+};

6abacefdcf9af4a131e25affa02468a36c7b4d40
deploy <deploy@users.noreply.github.com>
2025-11-30 10:56:32 +0000

    Updated

---
+++ ui@0.1.38/overrides/FigmaPlugin/exportImage.mjs
@@ -0,0 +1,10 @@
+import {
+  exportImage
+} from "../../chunk-NAN7VANW.mjs";
+import "../../chunk-7DA3HATF.mjs";
+import "../../chunk-OOWSMYW2.mjs";
+import "../../chunk-NP5XZW6Z.mjs";
+import "../../chunk-HXGKXP63.mjs";
+export {
+  exportImage
+};

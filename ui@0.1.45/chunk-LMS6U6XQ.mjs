1139dd0ef7b59a1d8540499ca5f568ece29e4bab
deploy <deploy@users.noreply.github.com>
2025-11-30 16:55:43 +0000

    Updated

---
+++ ui@0.1.45/chunk-LMS6U6XQ.mjs
@@ -0,0 +1,38 @@
+import {
+  useBillingInterval
+} from "./chunk-HADS6JZJ.mjs";
+import {
+  __spreadProps,
+  __spreadValues
+} from "./chunk-HXGKXP63.mjs";
+
+// src/overrides/Web/Pricing.tsx
+import { jsx } from "react/jsx-runtime";
+function TogglePrice(Component) {
+  return (props) => {
+    const [, setBillingInterval] = useBillingInterval();
+    return /* @__PURE__ */ jsx(
+      Component,
+      __spreadProps(__spreadValues({}, props), {
+        onMonthly: () => setBillingInterval("year"),
+        onYearly: () => setBillingInterval("month")
+      })
+    );
+  };
+}
+function Price(Component) {
+  return (props) => {
+    const [billingInterval] = useBillingInterval();
+    return /* @__PURE__ */ jsx(
+      Component,
+      __spreadProps(__spreadValues({}, props), {
+        variant: billingInterval === "year" ? "year" : "month"
+      })
+    );
+  };
+}
+
+export {
+  TogglePrice,
+  Price
+};

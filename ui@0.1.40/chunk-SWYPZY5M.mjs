01ad9610a23100b09260f30f670598ee91652040
deploy <deploy@users.noreply.github.com>
2025-11-30 12:07:40 +0000

    Updated

---
+++ ui@0.1.40/chunk-SWYPZY5M.mjs
@@ -0,0 +1,116 @@
+import {
+  NumberInput
+} from "./chunk-NQRDJNYL.mjs";
+import {
+  InputTitle
+} from "./chunk-SIKJUSOK.mjs";
+
+// src/components/FigmaPlugin/TripleNumberInput.tsx
+import { useState, useEffect } from "react";
+import { jsx, jsxs } from "react/jsx-runtime";
+function TripleNumberInput({
+  title,
+  defaultValueX,
+  defaultValueY,
+  defaultValueZ,
+  setValueX,
+  setValueY,
+  setValueZ,
+  step,
+  min,
+  max,
+  info,
+  infoContent,
+  condition
+}) {
+  const [sharedValueX, setSharedValueX] = useState(defaultValueX);
+  const [sharedValueY, setSharedValueY] = useState(defaultValueY);
+  const [sharedValueZ, setSharedValueZ] = useState(defaultValueZ);
+  const [mouseOverOn, setMouseOverOn] = useState("");
+  useEffect(() => {
+    setSharedValueX(defaultValueX);
+    setSharedValueY(defaultValueY);
+    setSharedValueZ(defaultValueZ);
+  }, []);
+  useEffect(() => {
+    setValueX(sharedValueX);
+  }, [sharedValueX]);
+  useEffect(() => {
+    setValueY(sharedValueY);
+  }, [sharedValueY]);
+  useEffect(() => {
+    setValueZ(sharedValueZ);
+  }, [sharedValueZ]);
+  useEffect(() => {
+    setSharedValueX(defaultValueX);
+  }, [defaultValueX]);
+  useEffect(() => {
+    setSharedValueY(defaultValueY);
+  }, [defaultValueY]);
+  useEffect(() => {
+    setSharedValueZ(defaultValueZ);
+  }, [defaultValueZ]);
+  return /* @__PURE__ */ jsxs(
+    "div",
+    {
+      className: "flex items-center w-full h-[26px] flex-row gap-2",
+      style: { fontFamily: "Inter Medium" },
+      children: [
+        /* @__PURE__ */ jsx(
+          InputTitle,
+          {
+            title,
+            info,
+            infoContent,
+            condition
+          }
+        ),
+        /* @__PURE__ */ jsxs("div", { className: "flex items-center w-full h-fit flex-row gap-2", children: [
+          /* @__PURE__ */ jsx(
+            NumberInput,
+            {
+              label: "x",
+              value: sharedValueX,
+              setValue: setSharedValueX,
+              mouseOverOn,
+              setMouseOverOn,
+              min,
+              max,
+              step
+            }
+          ),
+          /* @__PURE__ */ jsx(
+            NumberInput,
+            {
+              label: "y",
+              value: sharedValueY,
+              setValue: setSharedValueY,
+              mouseOverOn,
+              setMouseOverOn,
+              min,
+              max,
+              step
+            }
+          ),
+          /* @__PURE__ */ jsx(
+            NumberInput,
+            {
+              label: "z",
+              value: sharedValueZ,
+              setValue: setSharedValueZ,
+              mouseOverOn,
+              setMouseOverOn,
+              min,
+              max,
+              step
+            }
+          )
+        ] })
+      ]
+    }
+  );
+}
+
+export {
+  TripleNumberInput
+};
---
+++ ui@0.1.40/components/FigmaPlugin/InputTitle.mjs
@@ -0,0 +1,7 @@
+import {
+  InputTitle
+} from "../../chunk-SIKJUSOK.mjs";
+import "../../chunk-HXGKXP63.mjs";
+export {
+  InputTitle
+};

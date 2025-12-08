f2a8855bd0d2b4d6cff55d79b0192210c51d822b
deploy <deploy@users.noreply.github.com>
2025-12-08 05:56:30 +0000

    Updated

---
+++ ui@0.1.59/chunk-INPZIR7J.mjs
@@ -0,0 +1,62 @@
+import {
+  useSearchParamToStore
+} from "./chunk-YWVE66HW.mjs";
+import {
+  useControlValues
+} from "./chunk-JXGDOS3B.mjs";
+import {
+  init_store,
+  store_exports
+} from "./chunk-INPCX7NG.mjs";
+import {
+  ShaderGradient,
+  init_dist,
+  init_presetURLs,
+  propertyControls
+} from "./chunk-52FKGRQ2.mjs";
+import {
+  __spreadProps,
+  __spreadValues,
+  __toCommonJS
+} from "./chunk-HXGKXP63.mjs";
+
+// src/components/Shared/ShaderGradientStateless/ShaderGradientStateless.tsx
+init_dist();
+import { ControlType } from "framer";
+init_presetURLs();
+import { jsx } from "react/jsx-runtime";
+function ShaderGradientStateless(passedProps) {
+  useSearchParamToStore();
+  const props = useControlValues(passedProps.control, passedProps);
+  const enableCameraUpdate = passedProps.enableCameraUpdate !== false;
+  return /* @__PURE__ */ jsx(
+    ShaderGradient,
+    __spreadProps(__spreadValues({}, props), {
+      onCameraUpdate: enableCameraUpdate ? (updates) => {
+        const { cAzimuthAngle, cPolarAngle, cDistance, cameraZoom } = updates;
+        if (typeof window !== "undefined") {
+          const { useQueryStore } = (init_store(), __toCommonJS(store_exports));
+          const next = {};
+          if (typeof cAzimuthAngle !== "undefined")
+            next.cAzimuthAngle = cAzimuthAngle;
+          if (typeof cPolarAngle !== "undefined")
+            next.cPolarAngle = cPolarAngle;
+          if (typeof cDistance !== "undefined") next.cDistance = cDistance;
+          if (typeof cameraZoom !== "undefined")
+            next.cameraZoom = cameraZoom;
+          if (Object.keys(next).length) {
+            useQueryStore.setState(next);
+          }
+        }
+      } : void 0
+    })
+  );
+}
+ShaderGradientStateless.propertyControls = propertyControls(
+  ControlType,
+  "stateless"
+);
+
+export {
+  ShaderGradientStateless
+};
---
+++ ui@0.1.59/components/Shared/ShaderGradientStateless/store/store.mjs
@@ -0,0 +1,25 @@
+import {
+  init_store,
+  updateGradientState,
+  useBillingInterval,
+  useBillingIntervalStore,
+  useCursorStore,
+  useDomStore,
+  useFigma,
+  useQueryStore,
+  useUIStore
+} from "../../../../chunk-SKTC7ZLN.mjs";
+import "../../../../chunk-52FKGRQ2.mjs";
+import "../../../../chunk-NP5XZW6Z.mjs";
+import "../../../../chunk-HXGKXP63.mjs";
+init_store();
+export {
+  updateGradientState,
+  useBillingInterval,
+  useBillingIntervalStore,
+  useCursorStore,
+  useDomStore,
+  useFigma,
+  useQueryStore,
+  useUIStore
+};

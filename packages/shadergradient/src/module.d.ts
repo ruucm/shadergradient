88db262447ebeb0cdeda1a77435559fb036c822f
Zvi Karp <zvi@zektec.com>
2026-03-01 08:13:20 +0200

    fix: decouple framer typings from r3f v9 package deps

--- packages/shadergradient/src/module.d.ts
+++ packages/shadergradient/src/module.d.ts
@@ -2,3 +2,12 @@ declare module '*.glsl' {
   const value: string
   export default value
 }
+
+declare module 'framer' {
+  export const ControlType: Record<string, unknown>
+  export const RenderTarget: {
+    canvas: string
+    preview: string
+    current: () => string
+  }
+}
